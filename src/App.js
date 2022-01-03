607a0e1627508fdbec3572a93c184bc9ee8336fc
lance <kewei_lu@outlook.com>
2022-01-03 15:50:30 -0500

    add search box

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState } from "react";
+
+import NavigationHeader from "./components/layout/Navigation";
+import Search from "./components/Search";
 
 const App = () => {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchInput);
+  };
+
+  console.log(searchInput)
+
   return (
     <div className="App">
-      <h1>Image Gallery</h1>
+      <NavigationHeader title="Image Gallery" />
+      <Search
+        searchInput={searchInput}
+        setSearchInput={setSearchInput}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
-}
+};
 
 export default App;
