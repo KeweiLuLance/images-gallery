371f2840eb596d6bfc83d149fdb03db8120cad84
lance <kewei_lu@outlook.com>
2022-01-22 13:25:50 -0500

    saving images in the front app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,6 +48,23 @@ const App = () => {
     setImages(images.filter((image) => image.id !== id));
   };
 
+  const handleSaveImage = async (id) => {
+    const imageToBeSaved = images.find((image) => image.id === id);
+    imageToBeSaved.saved = true;
+    try {
+      const res = await axios.post(`${API_URL}/images`, imageToBeSaved);
+      if (res.data?.inserted_id) {
+        setImages(
+          images.map((image) =>
+            image.id === id ? { ...image, saved: true } : image
+          )
+        );
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="App">
       <NavigationHeader title="Image Gallery" />
@@ -65,6 +82,7 @@ const App = () => {
                   image={image}
                   key={i}
                   deleteImage={handleDeleteImage}
+                  saveImage={handleSaveImage}
                 />
               </Col>
             ))}
